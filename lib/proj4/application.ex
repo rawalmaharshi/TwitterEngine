d16ab2d96b5a61836a574042c9427bacee43d520
Maharshi Rawal <rawalmaharshi@gmail.com>
2019-11-21 17:33:39 -0500

    comments added to implement functionality

--- lib/proj4/application.ex
+++ lib/proj4/application.ex
@@ -6,14 +6,16 @@ defmodule Proj4.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      # Starts a worker by calling: Proj4.Worker.start_link(arg)
-      # {Proj4.Worker, arg}
-    ]
+    # children = [
+    #   # Starts a worker by calling: Proj4.Worker.start_link(arg)
+    #   # {Proj4.Worker, arg}
+    # ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Proj4.Supervisor]
-    Supervisor.start_link(children, opts)
+    # # See https://hexdocs.pm/elixir/Supervisor.html
+    # # for other strategies and supported options
+    # opts = [strategy: :one_for_one, name: Proj4.Supervisor]
+    # Supervisor.start_link(children, opts)
+
+    Proj4.Supervisor.start_link(self());
   end
 end
