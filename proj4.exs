5265b54cc22e252fbb70a2d56db301e9730d5477
Mohit Garg <mohitgarg@ufl.edu>
2019-11-24 11:13:00 -0500

    some change

--- proj4.exs
+++ proj4.exs
@@ -2,4 +2,4 @@ num_users = String.to_integer(Enum.at(System.argv(),0), 10)
 num_tweets = String.to_integer(Enum.at(System.argv(),1), 10)
 IO.inspect num_users
 IO.inspect num_tweets
-# Proj3.main([numNodes, numRequests])
+Proj4.main([num_users, num_tweets])
